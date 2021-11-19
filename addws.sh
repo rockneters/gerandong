89618729486aaa14ba538cf2b9dd413d949e9b52
rockneters <89226199+rockneters@users.noreply.github.com>
2021-11-19 16:28:59 +0700

    Add files via upload

---
+++ addws.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+
+domain=$(cat /etc/v2ray/domain)
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "User: " -e user
+		CLIENT_EXISTS=$(grep -w $user /etc/v2ray/config.json | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "A client with the specified name was already created, please choose another name."
+			exit 1
+		fi
+	done
+uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "Expired (days): " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#tls$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/config.json
+cat>/etc/v2ray/$user-tls.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "443",
+      "id": "${uuid}",
+      "aid": "2",
+      "net": "ws",
+      "path": "/v2ray",
+      "type": "none",
+      "host": "",
+      "tls": "tls"
+}
+EOF
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmesslink1="vmess://$(base64 -w 0 /etc/v2ray/$user-tls.json)"
+systemctl restart v2ray
+service cron restart
+clear
+echo -e ""
+echo -e "==========-V2RAY/VMESS-=========="
+echo -e "Remarks        : ${user}"
+echo -e "Domain         : ${domain}"
+echo -e "port TLS       : 443"
+echo -e "id             : ${uuid}"
+echo -e "alterId        : 2"
+echo -e "Security       : auto"
+echo -e "network        : ws"
+echo -e "path           : /v2ray"
+echo -e "================================="
+echo -e "link TLS       : ${vmesslink1}"
+echo -e "================================="
+echo -e "Expired On     : $exp"
